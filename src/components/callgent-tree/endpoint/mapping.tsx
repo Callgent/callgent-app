358ab85fdee29e8e8f9f25cd5a232d1b2df475b5
unknown <16632301768@163.com>
2025-07-21 08:01:56 +0800

    fix: Edit the apiMap parameters

--- src/components/callgent-tree/endpoint/mapping.tsx
+++ src/components/callgent-tree/endpoint/mapping.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef } from 'react'
-import { Input, InputRef, Divider } from 'antd'
+import { Input, InputRef, Tabs } from 'antd'
 import { useEndpointStore } from '@/models/endpoint'
 import EndpointSelectApi from './select-api'
 
@@ -11,23 +11,38 @@ export default function Mapping({ refs }: { refs: { parametersRef: any, requestB
   }, [])
 
   return (
-    <div>
-      <div>
-        <div className="font-medium bg-gray-50 py-2">
-          How2Ops
-        </div>
-        <Input.TextArea
-          rows={3}
-          disabled={status === 'edit'}
-          value={how2Ops}
-          className="w-full border border-gray-300  dark:border-gray-600 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
-          onChange={(e) => setHow2Ops(e.target.value)}
-        />
-      </div>
-      <Divider>OR</Divider>
-      <div>
-        <EndpointSelectApi refs={refs} />
-      </div>
-    </div>
+    <Tabs
+      tabPosition="left"
+      className="flex"
+      items={[
+        {
+          key: 'select',
+          label: 'Endpoint Select',
+          children: (
+            <div className="p-2">
+              <EndpointSelectApi refs={refs} />
+            </div>
+          ),
+        },
+        {
+          key: 'manual',
+          label: 'How2Ops',
+          children: (
+            <div className="p-2">
+              <div className="font-medium bg-gray-50 py-2">
+                How2Ops
+              </div>
+              <Input.TextArea
+                rows={3}
+                disabled={status === 'edit'}
+                value={how2Ops}
+                className="w-full border border-gray-300  dark:border-gray-600 rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-400"
+                onChange={(e) => setHow2Ops(e.target.value)}
+              />
+            </div>
+          ),
+        },
+      ]}
+    />
   )
 }
